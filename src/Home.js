52ca47653bc0d6b774a40efb02b008b1393a6271
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2023-08-10 08:55:07 -0300

    Added way to handle tokens

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,64 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleLogout = async () =>{
-    const logoutURL = "http://localhost:3500/logout";
-    try {
-        const response = await fetch(logoutURL,{
-            method:"POST",
-            headers:{
-                "Content-type":"application/json"
-            }, 
-        });
-        if(!response.ok){
-            const message = response.status;
-            throw new Error(message);
-        }
-        console.log("Sucessful logout ");
-        navigate("/");
-      } catch (error) {
-        console.log(error);
+    //This will get the passed values
+    const location = useLocation();
+    const auth = location.state.token;
+    
+    const handleLogout = async () =>{
+      const logoutURL = "http://localhost:3500/logout";
+      try {
+          const response = await fetch(logoutURL,{
+              method:"POST",
+              headers:{
+                  "Content-type":"application/json"
+              },
+          });
+
+          if (!response.ok){
+              const message = response.status;
+              throw new Error(message);
+          }
+          console.log("Sucessful logout ");
+          navigate("/");
+        } catch (error) {
+          console.log(error);
+      };
     };
-  };
-  
-  return (
-    <main>
-      <h2>Welcome back!!</h2>
-      <button onClick={handleLogout}>Logout</button>
-    </main>
-  )
-}
 
-export default Home
+    //Test function to test token management
+    const getUsers = async () =>{
+      const baseURL = "http://localhost:3500/api";
+      try {
+        const response = await fetch(baseURL,{
+          method: "GET",
+          headers:{
+            "Authorization": `Bearer ${auth}`,
+            "Content-Type": "application/json",
+          }
+        }); 
+        if (!response.ok){
+          navigate("/denied");
+          const message = response.status;
+          throw new Error(message);
+        }
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    
+    return (
+      <main>
+        <h2>Welcome back!!</h2>
+        <button onClick={getUsers}>Get users</button>
+        <button onClick={handleLogout}>Logout</button>
+      </main>
+    )
+  }
+
+  export default Home
--- src/Login.js
+++ src/Login.js
@@ -1,45 +1,47 @@
-import React from 'react'
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-const Login = ( {user, setUser} ) => {
-    const navigate = useNavigate();
-    
-    const handleChange =(event) =>{
-        const { name, value } = event.target;
-        setUser({
-          ...user,
-          [name]: value
-        });
-      }
 
-    const sendLoginRequest = async ()  =>{
-      const authURL = "http://localhost:3500/auth";
-        try {
-            const response = await fetch(authURL,{
-                method: "POST",
-                headers:{
-                    "Content-type":"application/json"
-                },
-                body:JSON.stringify(user)
-            });
-            if(!response.ok){
-                navigate("/denied");
-                const message = response.status;
-                throw new Error(message);
-            }
-            const data = await response.json();
-            console.log(data);
-            navigate("/home");
-        } catch (error) {
-            console.log(error);
-        }
+const Login = ( {user, setUser} ) => {
+  const navigate = useNavigate();
+
+  const handleChange =(event) =>{
+      const { name, value } = event.target;
+      setUser({
+        ...user,
+        [name]: value
+      });
     };
-    
-      const handleLogin = (event) =>{
-        event.preventDefault();
-        sendLoginRequest(user);
+
+  const sendLoginRequest = async ()  =>{
+    const authURL = "http://localhost:3500/auth";
+      try {
+          const response = await fetch(authURL,{
+              method: "POST",
+              headers:{
+                  "Content-type":"application/json"
+              },
+              body:JSON.stringify(user)
+          });
+          if (!response.ok){
+            navigate("/denied");
+            const message = response.status;
+            throw new Error(message);
+          }
+          const data = await response.json();
+          const token = data.accessToken;
+          console.log(token);
+          navigate("/home", {state: {token:token}});  
+        } catch (error) {
+          console.log(error);
       }
-      
+  };
+  
+    const handleLogin = (event) =>{
+      event.preventDefault();
+      sendLoginRequest(user);
+    }
+    
   return (
     <main>
         <h2>Login Page</h2>
