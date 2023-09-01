95c2a078e4c9fc77634d8e231358455739309af9
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2023-09-01 16:55:02 -0300

    Added a password validation
    On the the register page, I added a form validation for the password

--- src/Form.js
+++ src/Form.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = ({ user, setUser}) => {
-   const handleChange =(event) =>{
-      const { name, value } = event.target;
-      setUser({
-        ...user,
-        [name]: value
-      });
-    };
+  
+
+  const handleChange = (event) =>{
+    const { name, value } = event.target;
+    setUser({
+      ...user,
+      [name]: value  
+    });
+  };
     
-    return (
-        <form>         
-            <input 
-            type="text" 
-            name='userName'
-            value={user.userName}
-            onChange={handleChange}
-            />
-            <input 
-            type="password" 
-            name='password'
-            value={user.password}
-            onChange={handleChange}
-            />
-        </form>
+  return (
+    <form>
+      <label htmlFor="userName">User:</label>        
+      <input 
+      type="text" 
+      name='userName'
+      value={user.userName}
+      onChange={handleChange}
+      />
+      <label htmlFor="password">Password:</label>
+      <input 
+      type="password" 
+      name='password'
+      value={user.password}
+      onChange={handleChange}
+      />
+    </form>
   )
 }
 
--- src/Register.js
+++ src/Register.js
@@ -1,35 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Form from './Form';
 
 const Register = ({user, setUser}) => {
-
+  const [isValid, setIsValid] = useState(false);
+  const [validationError, setValidationError] = useState('');   //We can set the error message here, to display it to the user
   const navigate = useNavigate();
+
 
   const registerUser = async () =>{
     const baseURL = "http://localhost:3500/api";
     try {
         const response = await fetch(baseURL,{
-            method:"POST",
-            headers:{
-                "Content-type":"application/json"
-            }, 
-            body:JSON.stringify(user)
+          method:"POST",
+          headers:{
+              "Content-type":"application/json"
+          }, 
+          body:JSON.stringify(user)
         });
         
         if (!response.ok){
-            const message = response.status;
-            throw new Error(message);
+          const message = response.status;
+          throw new Error(message);
         }
         navigate("/home");
     } catch (error) {
         console.log(error);
     };
   };
+
+  const validateForm = () =>{
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    setIsValid(passwordPattern.test(user.password));
+    if (!isValid){
+      setValidationError("Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long.");
+    } else {
+      setValidationError('');
+      registerUser(user);
+    }
+  };
   
   const handleRegister =(event)=>{
     event.preventDefault();
-    registerUser(user);
+    validateForm();
   };
         
   return (
@@ -37,6 +50,7 @@ const Register = ({user, setUser}) => {
         <h2>Register page</h2>
         <Form user={user} setUser= {setUser} />
         <button onClick={handleRegister}>Register</button>
+        {isValid ? null : <p> {validationError} </p>}
     </main>
   )
 }
